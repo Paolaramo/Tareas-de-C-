a614f432d4c96891fbe09c8b6b4e320f677cc682
Paolaramo <98793749+Paolaramo@users.noreply.github.com>
2022-08-17 14:26:53 -0600

    Add files via upload

---
+++ TercerParcial/Tarea10/Ejercicio44/productos.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+extern void agregarProducto(string descripcion, int cantida, double precio);
+
+void productos(int opcion)
+{
+	system("cls");
+	
+int opcionProducto = 0;
+
+	switch(opcion)
+	{
+	case 1:
+	{
+		cout << "BEBIDAS CALIENTES " <<endl;
+		cout << "1 - Capuccino" <<endl;
+		cout << "2 - Expreso" <<endl;
+	    
+	    cout << " Ingrese una opción: ";
+		cin >> opcionProducto;
+		
+		switch(opcionProducto)
+		{
+		case 1:
+			agregarProducto("Capuccino L.30.00", 1, 30);
+			break;
+		case 2:
+			agregarProducto("Expresso L-40.00", 1, 40);
+			break;
+				
+		default:
+			{
+			cout << "opcion no valida";
+			return;
+			break;
+			}	
+		}
+			
+		cout <<endl;
+		cout << "Producto agregado" <<endl <<endl;
+		system("pause");
+	
+		break;
+	}
+	case 2:
+	{	
+		cout << "BEBIDAS FRIAS" <<endl;
+		system("pause");
+		break;
+	}
+	case 3:
+	{	
+		cout << "REPOSTERIA" <<endl;
+		system("pause");
+		break;
+	}
+	
+	default:
+		break;
+	}
+		
+}
